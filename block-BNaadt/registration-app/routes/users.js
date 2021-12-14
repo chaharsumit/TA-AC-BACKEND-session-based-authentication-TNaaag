49ac23b367f904b01626d36dedd6d67ebd2a747b
Sumit Chahar <chahar.sumit888@gmail.com>
2021-12-14 19:42:47 +0530

    Add user login authentication and flash messages for failed validation checks

---
+++ block-BNaadt/registration-app/routes/users.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var User = require('../models/user');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('users');
+});
+
+router.get('/login', (req, res, next) => {
+  let error = req.flash("error");
+  res.render('login', { error });
+})
+
+router.post('/login', (req, res, next) => {
+  var { email, password } = req.body;
+  if(!email || !password){
+    req.flash("error", "email/password required");
+    return res.redirect('/users/login');
+  }
+  User.findOne({ email }, (err, user) => {
+    if(err){
+      return next(err);
+    }
+    if(!user){
+      req.flash("error", "register user before attempting login");
+      return res.redirect('/users/register');
+    }
+    user.verifyPassword(password, (err, result) => {
+      if(err){
+        return next(err);
+      }
+      if(!result){
+        req.flash("error", "password verification failed");
+        return res.redirect('/users/login');
+      }
+      req.session.userId = user.id;
+      res.redirect('/');
+    })
+  })
+})
+
+router.get('/register', (req, res, next) => {
+  let error = req.flash('error');
+  res.render('register', { error });
+})
+
+router.post('/register', (req, res, next) => {
+  User.create(req.body, (err, user) => {
+    if(err){
+      return next(err);
+    }
+    res.redirect('/');
+  })
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.clearCookie("connect.sid");
+  res.redirect('/users');
+})
+
+module.exports = router;
